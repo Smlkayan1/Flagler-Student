e4be5a68fc0eb87f579151ad7b591362d02d5039
Selma Kayan <selmakayan@gmail.com>
2024-01-25 11:23:36 -0500

    Initial Commit

---
+++ Flagler Student/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flagler Student
+//
+//  Created by Selma Kayan on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
